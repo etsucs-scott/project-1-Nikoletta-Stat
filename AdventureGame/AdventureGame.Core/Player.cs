f41a4d15665699966389f397b5ac334bef3d99f0
Nikoletta-Stat <STATHOUDAKIN@ETSU.EDU>
2026-02-10 21:22:03 -0500

    Committed

---
+++ AdventureGame/AdventureGame.Core/Player.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventureGame.Core
+{
+    public class Player : ICharacter
+    {
+        public int Health { get; set; } = 100;
+        public int damage { get; set; } = 10;
+
+        public void Attack(ICharacter target)
+        {
+            target.TakeDamage(damage);
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+        }
+    }
+}
